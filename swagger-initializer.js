a55d93feddb68471d173260af0c3e784927e3638
sashwath-kumar <sashwath-kumar@users.noreply.github.com>
2025-03-19 08:41:40 +0000

    deploy: 343d5b8c2f947781a4a486691bcc9e878f03884c

---
+++ swagger-initializer.js
@@ -0,0 +1,22 @@
+window.onload = function() {
+  window.ui = SwaggerUIBundle({
+    url: "./openapi.json",
+    dom_id: '#swagger-ui',
+    deepLinking: true,
+    presets: [
+      SwaggerUIBundle.presets.apis,
+      SwaggerUIStandalonePreset
+    ],
+    plugins: [
+      SwaggerUIBundle.plugins.DownloadUrl
+    ],
+    layout: "StandaloneLayout",
+    requestInterceptor: function(request) {
+      const apiUrl = 'https://sashwath-kumar.github.io/Leaderboard/'; 
+      if (request.url.startsWith('/')) {
+        request.url = apiUrl + request.url;
+      }
+      return request;
+    }
+  });
+};
